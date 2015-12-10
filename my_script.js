bfdf17e43357f6f8553de14dc14d0c6112b56077
Michael Wang <michael.icwang@gmail.com>
2015-12-10 11:57:57 +0800

    Add rank by metrics.

--- my_script.js
+++ my_script.js
@@ -1,3 +1,4 @@
+/* Planet index */
 const MERCURY = 0;
 const VENUS   = 1;
 const EARTH   = 2;
@@ -30,9 +31,10 @@ IMG[URANUS]  = 'images/uranus.jpg';
 IMG[NEPTUNE] = 'images/neptune.jpg';
 IMG[PLUTO]   = 'images/pluto.jpg';
 
-var RANK = [MERCURY, VENUS, EARTH, MARS, JUPITER, SATURN, URANUS, NEPTUNE, PLUTO];
 
 function show_planets() {
+  var RANK = RANKS[metric_index];
+
   for (rank_index = 0; rank_index < RANK.length; rank_index++) {
     var planet_index = RANK[rank_index];
 
@@ -44,17 +46,45 @@ function show_planets() {
   }
 }
 
-var metric_index = 4;
+/* Metric index */
+const MASS          = 0;
+const DIAMETER      = 1;
+const GRAVITY       = 2;
+const LEN_OF_DAY    = 3;
+const DIST_FROM_SUN = 4;
+const ORB_PERIOD    = 5;
+const NUM_OF_MOON   = 6;
+
+var RANK_BY_MASS     = [JUPITER, SATURN, NEPTUNE, URANUS, EARTH, VENUS, MARS, MERCURY, PLUTO];
+var RANK_BY_DIAMETER = [JUPITER, SATURN, URANUS, NEPTUNE, EARTH, VENUS, MARS, MERCURY, PLUTO];
+var RANK_BY_GRAVITY  = [JUPITER, NEPTUNE, EARTH, SATURN, VENUS, URANUS, MERCURY, MARS, PLUTO];
+var RANK_BY_LENOFDAY = [MERCURY, VENUS, PLUTO, MARS, EARTH, URANUS, NEPTUNE, SATURN, JUPITER];
+var RANK_BY_DISTSUN  = [MERCURY, VENUS, EARTH, MARS, JUPITER, SATURN, URANUS, NEPTUNE, PLUTO];
+var RANK_BY_ORBPERIOD= [PLUTO, NEPTUNE, URANUS, SATURN, JUPITER, MARS, EARTH, VENUS, MERCURY];
+var RANK_BY_NUMMOON  = [JUPITER, SATURN, URANUS, NEPTUNE, PLUTO, MARS, EARTH, MERCURY, VENUS];
+
+var RANKS = []
+RANKS[MASS]          = RANK_BY_MASS;
+RANKS[DIAMETER]      = RANK_BY_DIAMETER;
+RANKS[GRAVITY]       = RANK_BY_GRAVITY;
+RANKS[LEN_OF_DAY]    = RANK_BY_LENOFDAY;
+RANKS[DIST_FROM_SUN] = RANK_BY_DISTSUN;
+RANKS[ORB_PERIOD]    = RANK_BY_ORBPERIOD;
+RANKS[NUM_OF_MOON]   = RANK_BY_NUMMOON;
+
+var metric_index = DIST_FROM_SUN;
 
 function select_metric() {
-  $('[id^=metric]').removeClass('active');
+  $('[id^=metric-]').removeClass('active');
   $(this).addClass('active');
   metric_index = parseInt($(this).attr('id').slice(-1));
   console.log('metric_index: ', metric_index);
+
+  show_planets();
 }
 
 function main() {
-  $('[id^=metric]').click(select_metric);
+  $('[id^=metric-]').click(select_metric);
 
   show_planets();
 }
